742f0f05f13c8de07e84040ae9fb5b771164cd92
Bijaya Khadka <bijaya.khadka@fusemachines.com>
2025-01-09 20:17:24 +0545

    [update] added source filter

--- app.py
+++ app.py
@@ -11,19 +11,37 @@ app = Flask(__name__)
 def index():
     page = request.args.get('page', 1, type=int)
     per_page = 10
-    articles = get_articles()
+    articles, sources = get_articles()
 
-    paginated_articales = paginate(page, per_page, articles)
+    paginated_articles = paginate(page, per_page, articles)
 
-    return render_template('index.html', articles=paginated_articales,
+    return render_template('index.html', articles=paginated_articles,categories=sources,
                            page=page, total_pages=len(articles)//per_page + 1)
-
-
 
 @app.route("/search")
 def search():
     query = request.args.get('q')
 
-    articles = get_articles(query)
+    articles, sources = get_articles(query)
 
-    return render_template('search_results.html', articles=articles, query=query)
+    return render_template('search_results.html', articles=articles, categories=sources,query=query)
+
+
+@app.route("/filter")
+def filter_items():
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    selected_source = request.args.get('category', 'All')
+
+    if selected_source == "All":
+        articles, sources = get_articles()
+    else:
+        articles, sources = get_articles(source = selected_source)
+
+    paginated_articles = paginate(page, per_page, articles)
+
+    return render_template('index.html', articles=paginated_articles,categories=sources,
+                           page=page, total_pages=len(articles)//per_page + 1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/fetch_feeds.py
+++ src/fetch_feeds.py
@@ -2,12 +2,19 @@ import feedparser
 from src.rss_links import RSS_FEEDS, RSS_FEEDS_AI
 
 
-def get_articles(query: str | None = None):
+def get_articles(query: str | None = None, source : str = "All"):
     articles = []
-
-    for source, feed in RSS_FEEDS_AI.items():
+    
+    for site, feed in RSS_FEEDS_AI.items():
+        if source != "All":
+            selected_feed = RSS_FEEDS_AI.get(source)
+            print(f'{selected_feed=}')
+            parsed_feed = feedparser.parse(selected_feed)
+            entries = [(source, entry) for entry in parsed_feed.entries]
+            articles.extend(entries)
+            break
         parsed_feed = feedparser.parse(feed)
-        entries = [(source, entry) for entry in parsed_feed.entries]
+        entries = [(site, entry) for entry in parsed_feed.entries]
         articles.extend(entries)
 
     articles = sorted(articles, key = lambda x: x[1].published_parsed, reverse=True) 
@@ -16,7 +23,7 @@ def get_articles(query: str | None = None):
         results = [article for article in articles if query.lower() in article[1].title.lower()]
         return results
     
-    return articles
+    return articles, list(RSS_FEEDS_AI.keys())
 
 
 
